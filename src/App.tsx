0a0b487517b40d8ec22d1894794d352df6d5abcb
devthiago <contatothiagoalbuquerque@gmail.com>
2024-04-03 18:38:07 -0300

    feat: car detail page creation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 import AuthProvider from "./contexts/AuthContext";
 
+import { register } from "swiper/element/bundle";
+
+register();
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 export default function App() {
   return (
     <AuthProvider>
--- src/pages/carDetails/carDetails.tsx
+++ src/pages/carDetails/carDetails.tsx
@@ -1,8 +1,147 @@
+import { useEffect, useState } from "react";
+import { MainContainer } from "../../components/MainContainer/MainContainer";
+import { FaWhatsapp } from "react-icons/fa";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { getDoc, doc } from "firebase/firestore";
+import { dataBase } from "../../services/firebaseServices";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+interface CarProps {
+  id: string;
+  name: string;
+  model: string;
+  city: string;
+  year: string;
+  km: string;
+  description: string;
+  created: string;
+  price: string | number;
+  owner: string;
+  uid: string;
+  whatsapp: string;
+  images: ImageCarProps[];
+}
+
+interface ImageCarProps {
+  uid: string;
+  name: string;
+  url: string;
+}
+
 function carDetails() {
+  const { id } = useParams();
+  const [car, setCar] = useState<CarProps>();
+  const [sliderPerView, setSliderPerView] = useState<number>(2);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadCar() {
+      if (!id) {
+        return;
+      }
+
+      const docRef = doc(dataBase, "cars", id);
+      getDoc(docRef).then((snapshot) => {
+        if (!snapshot.data()) {
+          navigate("/");
+        }
+
+        setCar({
+          id: snapshot.id,
+          name: snapshot.data()?.name,
+          year: snapshot.data()?.year,
+          city: snapshot.data()?.city,
+          model: snapshot.data()?.model,
+          uid: snapshot.data()?.uid,
+          description: snapshot.data()?.description,
+          created: snapshot.data()?.created,
+          whatsapp: snapshot.data()?.whatsapp,
+          price: snapshot.data()?.price,
+          km: snapshot.data()?.km,
+          owner: snapshot.data()?.owner,
+          images: snapshot.data()?.images,
+        });
+      });
+    }
+
+    loadCar();
+  }, [id]);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 720) {
+        setSliderPerView(1);
+      } else {
+        setSliderPerView(2);
+      }
+    }
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>carDetails</h1>
-    </div>
+    <MainContainer>
+      <Swiper
+        slidesPerView={sliderPerView}
+        pagination={{ clickable: true }}
+        navigation
+      >
+        {car?.images.map((image) => (
+          <SwiperSlide key={image.name}>
+            <img src={image.url} className="w-full h-96 object-cover" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {car && (
+        <section className="w-full bg-white rounded-lg p-6 my-4">
+          <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
+            <h1 className="font-bold text-3xl text-black">{car?.name}</h1>
+            <h1 className="font-bold text-3xl text-black">{car?.price}</h1>
+          </div>
+
+          <p>{car?.model}</p>
+
+          <div className="flex w-full gap-6 my-4">
+            <div className="flex flex-col gap-4">
+              <div>
+                <p>Cidade</p>
+                <strong>{car?.city}</strong>
+              </div>
+              <div>
+                <p>Ano</p>
+                <strong>{car?.year}</strong>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div>
+                <p>KM</p>
+                <strong>{car?.km}</strong>
+              </div>
+            </div>
+          </div>
+
+          <strong>Descrição</strong>
+          <p className="mb-4">{car?.description}</p>
+
+          <strong>Contato</strong>
+          <p className="mb-4">{car?.whatsapp}</p>
+
+          <a className="bg-green-500 max-w-lg text-white flex items-center justify-center gap-2 my-6 h-11 text-x1 rounded-lg font-medium cursor-pointer m-auto">
+            Conversar com o vendedor
+            <FaWhatsapp size={26} color="#fff" />
+          </a>
+        </section>
+      )}
+    </MainContainer>
   );
 }
 
